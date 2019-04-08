4078d4042945168f102032f54be407af6a2e7a8b
akruti <akrutichavre21@gmail.com>
2019-04-08 18:54:58 +0530

    combobox added

--- src/main/java/database_schema/DatabaseSchema.java
+++ src/main/java/database_schema/DatabaseSchema.java
@@ -1,17 +1,25 @@
 package database_schema;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.*;
 import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 
 
 public class DatabaseSchema {
 	
-	
+	static ArrayList<String> DatabaseNames = new ArrayList<String>();
 	
 	public static void main(String args[]) {
 		Connection myconn=null;
 		DatabaseMetaData meta = null;
+		//final String data = "";
 		try
 		{
 			//1. Get connection
@@ -21,11 +29,45 @@ public class DatabaseSchema {
 			meta = myconn.getMetaData();
 			ResultSet catalogs = meta.getCatalogs();
 			
-			while (catalogs.next()) {
-			       String catalog = catalogs.getString(1);  //"TABLE_CATALOG"
-			       System.out.println("catalog: "+catalog);
-			     }
+			
+			
+			while(catalogs.next())
+			{
+				String catalog = catalogs.getString(1);
+			//	System.out.println(catalog);
+				DatabaseNames.add(catalog);
+			}
+			
+			System.out.println(DatabaseNames);
+			
+			JFrame f;    
+			
+			
+			f=new JFrame("Databases - ");   
+			final JLabel label = new JLabel();          
+			label.setHorizontalAlignment(JLabel.CENTER);  
+		    label.setSize(400,100);  
+		    JButton b=new JButton("Show");  
+		    b.setBounds(200,100,75,20);  	            
+		    final JComboBox cb=new JComboBox(DatabaseNames.toArray());    
+			cb.setBounds(50, 100,90,20);    
+			f.add(cb); f.add(label); f.add(b);    
+			f.setLayout(null);    
+			f.setSize(350,350);    
+			f.setVisible(true);   
+			
+			b.addActionListener(new ActionListener() {  
+				public void actionPerformed(ActionEvent e) {   
+				String data = ""+cb.getItemAt(cb.getSelectedIndex());  
+				String localData = "Database selected: " + data;
+				label.setText(localData); 
+				
+				System.out.println(data);
+			
+				}
+			});
 		}
+		
 		catch(Exception e)
 		{
 			e.printStackTrace();
